9afa5ad9fbe164d13a02477af9ba5b2d6f827816
mir-am <mir-am@hotmail.com>
2018-11-12 12:24:59 +0330

    A function for finding alignments from a BLAST XML file.

--- src/pssm_clc.py
+++ src/pssm_clc.py
@@ -11,28 +11,49 @@ from Bio.Blast import NCBIXML
 
 pro_seq = """PIVDTGSVAPLSAAEKTKIRSAWAPVYSTYETSGVDILVKFFTSTPAAQEFFPKFKGLTTADELKKSADVRWHAERIINAVDDAVASMDDTEKMSMKLRNLSGKHAKSFQVDPEYFKVLAAVIADTVAAGDAGFEKLMSMICILLRSAY"""
 
-blast_qresult = open("../data/2LHB-BLAST.xml")
-blast_record = NCBIXML.read(blast_qresult)
-blast_records = NCBIXML.parse(blast_qresult)
 
-E_VALUE_THRESH = 0.04
+def find_aligments(blast_xml, e_thresh=0.04):
+    
+    """
+    It finds Aligments for a givem BLAST XML file
+    
+    Input:
+        blast_xml: BLAST XML file
+        
+    return:
+        Alighments Query and Match (List, List)
+    """
+    
+    blast_qresult = open(blast_xml)
+    
+    blast_record = NCBIXML.read(blast_qresult)
+    blast_records = NCBIXML.parse(blast_qresult)
+    
+    E_VALUE_THRESH = e_thresh
+    
+    alignments = []
+    alignment_match = []
+    alignment_query = []
+    
+    # Find alignments of protein sequence and extract 
+    for alignment in blast_record.alignments:
+    
+          for hsp in alignment.hsps:
+    
+               if hsp.expect < E_VALUE_THRESH:
+                   
+               # only consider protein sequence query   
+                   if hsp.query == pro_seq:
+    
+                    alignments.append(str(hsp.sbjct[0:]))
+                    alignment_match.append(hsp.match)
+                    alignment_query.append(hsp.query)
+                    
+    blast_qresult.close()
+                    
+    return alignment_match, alignment_query
 
-alignments = []
-alignment_match = []
-alignment_query = []
 
-# Find alignments of protein sequence and extract 
-for alignment in blast_record.alignments:
-
-      for hsp in alignment.hsps:
-
-           if hsp.expect < E_VALUE_THRESH:
-               
-           # only consider protein sequence query   
-               if hsp.query == pro_seq:
-
-                alignments.append(str(hsp.sbjct[0:]))
-                alignment_match.append(hsp.match)
-                alignment_query.append(hsp.query)
-
-
+if __name__ == '__main__':
+    
+    query, match = find_aligments("../data/2LHB-BLAST.xml")
