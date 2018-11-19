6e35be08169509499c89ab4759af10de83f1acb8
mir-am <mir-am@hotmail.com>
2018-11-19 18:50:57 +0330

    Confusion matrix and classification report for SVM & Bigram features.

--- src/misc.py
+++ src/misc.py
@@ -28,13 +28,13 @@ def plt_confusion_matrix(cm, classes, title='Confusion matrix',
     plt.xticks(tick_marks, classes, rotation=45)
     plt.yticks(tick_marks, classes, rotation=45)
     
-    fmt = "%.2f"
+    fmt = '.2f'
     thresh = cm.max() / 2.0
     
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
         
-        plt.text(j, i, format(cm[i, j], fmt), horizontalalignment="center",
-                 color="white" if cm[i, j] > thresh else "balck")
+        plt.text(j, i, format(cm[i, j], fmt), horizontalalignment="center", \
+                 color="white" if cm[i, j] > thresh else "black")
         
     plt.ylabel("True label")
     plt.xlabel("Predicted label")
