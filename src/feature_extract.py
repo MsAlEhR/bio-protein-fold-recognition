94623a786237add616ac996a40c1a468611f1293
MsAlEhR <mohammadsaleh.refahi@gmail.com>
2018-11-27 13:36:04 +0330

    ACC feature extraction method and dataset added

--- src/feature_extract.py
+++ src/feature_extract.py
@@ -40,7 +40,7 @@ def bigram(pssm_matrix,k=1):
 
 def create_dataset(protein_dtframe,function,k,dataset_name):
     
-    columns = ['F' + str(i) for i in range(1,401)]
+    columns = ['F' + str(i) for i in range(1,1601)]
     
     data_set = pd.DataFrame(columns=['Fold','Protein name'] + columns)
     
@@ -90,10 +90,65 @@ def separated_dimer(pssm_matrix,K):
             
     return spr_matrix    
     
+def acc_transformation(pssm_matrix,K):
     
+    """
+    Get Feature by ACC Transformation 
+    
+    Input : PSSM matrix , K = LG
+    
+    Output : Feature vector(20*LG + 380*LG)
+    """
+    
+    amino_acides = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N',
+                    'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y']
+        
+    pssm_matrix = pd.read_csv(pssm_matrix,sep=',',names =amino_acides)
+    
+    AC_matrix = []        
+    CC_matrix = []
+    
+    for k in range(1,K+1) :
+        
+        
+        # calculate eh mean of any amino acid clomn in every 
+    
+        average = pssm_matrix[amino_acides].mean()
+        
+        for x in amino_acides:
+                
+                ac_sum = 0
+                
+                for i in range(1,pssm_matrix.shape[0]-k):
+                    
+                    ac_sum = ac_sum + (pssm_matrix[x][i] - average[x]) * (pssm_matrix[x][i+k]-average[x])/(pssm_matrix.shape[0]-k)
+                        
+                AC_matrix.append(ac_sum)
+        
+        
+        for x in amino_acides:
+          
+            for y in amino_acides :
+                
+                if x == y :
+                    continue
+                
+                cc_sum =0
+                
+                for i in range(1,pssm_matrix.shape[0]-k):
+                    
+                    cc_sum = cc_sum + (pssm_matrix[x][i] - average[x]) * (pssm_matrix[y][i+k] - average[y])/(pssm_matrix.shape[0]-k)
+                CC_matrix.append(cc_sum)
+    
+    return AC_matrix + CC_matrix
+    
+        
 
 if __name__ == '__main__':
     
     protein_dtfrm = pd.read_csv(r"./dataset/DD_raw.csv")
     
-    data = create_dataset(protein_dtfrm,separated_dimer,7,'separated_dimer_dataset')    
+    data = create_dataset(protein_dtfrm,acc_transformation,4,'ACC_dataset')  
+    
+    
+    
