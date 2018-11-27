2ef40edc0aed66c0c850261f55b91d3b5b41e1c0
MsAlEhR <mohammadsaleh.refahi@gmail.com>
2018-11-27 17:34:49 +0330

    Converting dataset to tsv

---
+++ src/tsv_dataset.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 27 15:07:56 2018
+
+@author: Saleh , Mir, A. 
+
+"""
+
+from os.path import splitext
+import pandas as pd 
+
+def create_tsv(dataset_name):
+   
+    """
+      Create .tsv files for label and features
+      
+      Input : Dataset name , and number of dataset
+      
+      Output : Label , feature in .tsv files
+    
+    """
+
+    
+    dataset = pd.read_csv('./dataset/'+ dataset_name)
+    
+    no_samples, no_features = dataset.shape
+    no_features = no_features - 2
+    
+     
+    y_true, labels = pd.factorize(dataset.Fold)
+    dataset.insert(1, 'class labels', y_true)
+    
+    dataset[['Protein name','class labels']].to_csv('./dataset/' + splitext(dataset_name)[0] + \
+           '_label' + '.txt', index=False, header=False, sep="\t")
+    
+    dataset[['Protein name']+['F%d' % i for i in range(1, no_features + 1)]].to_csv('./dataset/' + \
+            splitext(dataset_name)[0] + '_feature' + '.tsv', index=False, sep="\t")
+    
+    return dataset,dataset[['Protein name','class labels']],dataset[['Protein name']+ \
+                           ['F%d' % i for i in range(1, no_features + 1)]]
+
+if __name__ == "__main__":
+         
+    dataset, label , feature = create_tsv('ACC_dataset.csv')
