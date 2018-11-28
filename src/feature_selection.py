b6ac79b31947b0edb27c8eb989a34e55822e4289
mir-am <mir-am@hotmail.com>
2018-11-28 18:11:45 +0330

    Expremints with fusion features (ACC + Dimers).

--- src/feature_selection.py
+++ src/feature_selection.py
@@ -5,6 +5,8 @@ Created on Wed Nov 28 11:11:38 2018
 @author: Saleh, Mir, A.
 """
 
+import csv
+import re
 import pandas as pd
 
 
@@ -27,6 +29,31 @@ def gen_slc_feature(data_name,slc_feature,output_name,no_feature):
     return  dataset[['Fold','Protein name']+list(slct_feature[0][:no_feature])]
 
 
+def fusion_feature(data_name1, data_name2, merged_data_name):
+    
+    
+    dataset1 = pd.read_csv('./dataset/' + data_name1, sep=",", skiprows=[0],
+                           header=None)
+    
+    dataset2 = pd.read_csv('./dataset/' + data_name2, sep=",")
+    
+    # Find number of features in dataset2
+    d2_features = [col for col in list(dataset2.columns.values) if bool(re.match('F\d+',
+                   str(col)))]
+    
+    # Number of features in merged dataframe    
+    new_dim = (dataset1.shape[1] - 2) + len(d2_features)
+    
+        
+    result = pd.concat([dataset1, dataset2[d2_features]], axis=1, sort=False)
+    
+    result.to_csv('./dataset/dd_fusion_acc_dimer.csv', index=False, \
+                  header=['Fold', 'Protein name'] + ['F%d' % i for i in range(1, new_dim + 1)])
+    
+    return result 
+
 if __name__ == "__main__":
     
-    dtst = gen_slc_feature('ACC_dataset.csv','ACC_IG_feature.txt','ACC_dataset_IG.csv',500)
+    df = fusion_feature("ACC_dataset_IG.csv","pca_spd.csv", 'dd_fusion_acc_dimer.csv')
+    
+    #dtst = gen_slc_feature('ACC_dataset.csv','ACC_IG_feature.txt','ACC_dataset_IG.csv',500)
--- src/report/experiments.md
+++ src/report/experiments.md
@@ -9,6 +9,7 @@ One-vs-One is used for multiclassification
 | PSSM + Bigram + SVM   		 | 73.25    |
 | Separate dimers + Linear TSVM  | 75.88    |
 | Separate dimers + SVM 		 | 85.20    |
+| ACC + dimers + IG + SVM 		 | 86.17   |
 | ACC features + SVM             | 89.06    |
 | ACC + IG + SVM                 | 91.0     |
 
